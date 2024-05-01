ef97c4e432c43bb874aa85b8d9c3a855c9b98be5
HasanGhavisheh <ecma.work@gmail.com>
2024-05-01 17:37:15 +0330

    Complete Back-end

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -1,7 +1,12 @@
-﻿namespace ColorMaker;
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace ColorMaker;
 
 public partial class MainPage : ContentPage
 {
+    private readonly Random _rnd = new();
+    private bool _isRandom = false;
+
     public MainPage()
     {
         InitializeComponent();
@@ -9,12 +14,46 @@ public partial class MainPage : ContentPage
 
     private void Sld_ValueChanged(object sender, ValueChangedEventArgs e)
     {
+        if (_isRandom)
+            return;
+            
+        var red = SldRed.Value;
+        var green = SldGreen.Value;
+        var blue = SldBlue.Value;
 
+        Color color = Color.FromRgb(red, green, blue);
+
+        SetColor(color);
     }
 
-    private void ImageButton_Clicked(object sender, EventArgs e)
+    private void SetColor(Color color)
     {
+        LblHex.Text = color.ToHex();
+        Container.BackgroundColor = color;
+        BtnRandom.BackgroundColor = color;
+    }
 
+    private void GenerateRandomColor(object sender, EventArgs e)
+    {
+        _isRandom = true;
+
+        Color randColor = Color.FromRgb(_rnd.Next(256), _rnd.Next(256), _rnd.Next(256));
+
+        SldRed.Value = randColor.Red;
+        SldGreen.Value = randColor.Green;
+        SldBlue.Value = randColor.Blue;
+
+        SetColor(randColor);
+        _isRandom = false;
+    }
+
+    private async void CopyToClipboard(object sender, EventArgs e)
+    {
+        var color = LblHex.Text;
+        await Clipboard.SetTextAsync(color);
+        var toast = Toast.Make("Color Copied", CommunityToolkit.Maui.Core.ToastDuration.Short, 13);
+
+        await toast.Show();
     }
 }
 
--- ColorMaker/MauiProgram.cs
+++ ColorMaker/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace ColorMaker;
 public static class MauiProgram
@@ -6,13 +7,16 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
+            builder
             .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+
+
 
 #if DEBUG
 		builder.Logging.AddDebug();
